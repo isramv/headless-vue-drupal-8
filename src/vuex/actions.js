b98867794f2ab10bd61b67d99da6af9320194fcb
Israel Morales <israel.morales@rocket.com.mx>
2016-12-02 18:09:54 -0800

    Incremental update

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const actions = {
   getMorePosts (context) {
-    console.log('getting more posts... boss')
-    console.log(context.getters.nextPage)
+    // console.log('getting more posts... boss')
+    // console.log(context.getters.nextPage)
     context.commit('setUpdating', true)
     axios.get('//dev.chapterthree.com/api/1.1/blog/?page=' + context.getters.nextPage)
       .then((response) => {
@@ -12,6 +12,7 @@ const actions = {
           context.commit('setUpdating', false)
         })
         /* eslint-disable no-undef */
+        localStorage.setItem('posts', JSON.stringify(context.state.posts))
         // localStorage.setItem('posts', JSON.stringify(response.data))
         // this.$store.commit('setPosts', response.data)
       })
